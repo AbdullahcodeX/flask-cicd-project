9c2b942709dda66b6c4e56fd06a2ea9c136b8535
AbdullahcodeX <you@example.com>
2026-01-30 12:24:15 +0500

    git pull

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ PORT = int(os.getenv("APP_PORT", 4000))
 
 @app.route("/")
 def home():
+<<<<<<< HEAD
     if ENVIRONMENT == "live":
         return "Welcome to Live"
     return "Welcome to Dev 44314 23423"
+=======
+
+    if ENVIRONMENT == "live":
+
+        return "Welcome to Live"
+
+    return "Welcome to Dev to dev server"
+>>>>>>> a53478b (add code)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
