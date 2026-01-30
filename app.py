aa6b9ecbdc219c4e313f1a44427af4a2e395db37
EC2 Default User <ec2-user@ip-172-31-34-182.eu-north-1.compute.internal>
2026-01-30 13:17:05 +0000

    Abdullah update this file

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 ENVIRONMENT = os.getenv("APP_ENV", "dev")
-PORT = int(os.getenv("APP_PORT", 4000))
+PORT = int(os.getenv("APP_PORT", 5000))
 
 @app.route("/")
 def home():
     if ENVIRONMENT == "live":
-        return "Welcome to Live"
-    return "Welcome to Dev"
+        return "Abdullah how are you"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
